0f41d52e1364aa6b361b7e7acdf11de03f07e242
zeyaurrahman786 <zeyaurrahman078608@gmail.com>
2024-06-04 00:12:02 +0530

    MongoDB

--- MongoDB/index.js
+++ MongoDB/index.js
@@ -47,13 +47,47 @@ const User = mongoose.model("User", userSchema);
 
 
 
-User.insertMany([
-  {name: 'John', email : 'john@example.com', age: 50},
-  {name: 'Tony', email : 'tony@example.com', age: 40},
-  {name: 'Peter', email : 'peter@example.com', age: 45},
-  {name: 'Bruce', email : 'bruce@example.com', age: 55},
-  {name: 'Adam', email : 'adam@example.com', age: 60},
-])
-.then((res) => {
+// User.insertMany([
+//   {name: 'John', email : 'john@example.com', age: 50},
+//   {name: 'Tony', email : 'tony@example.com', age: 40},
+//   {name: 'Peter', email : 'peter@example.com', age: 45},
+//   {name: 'Bruce', email : 'bruce@example.com', age: 55},
+//   {name: 'Adam', email : 'adam@example.com', age: 60},
+// ])
+// .then((res) => {
+//   console.log(res);
+// });
+
+
+
+
+// User.find({age: {$gt: 45}})
+// .then((res)=>{
+//   console.log(res[0].name);
+// })
+// .catch((err)=>{
+//   console.log(err);
+// })
+
+
+
+
+
+// User.findOne({age: {$gt: 45}})
+// .then((res)=>{
+//   console.log(res);
+// })
+// .catch((err)=>{
+//   console.log(err);
+// })
+
+
+
+
+User.findById({_id: '665e006d84ecec3f65ac8f5a'})
+.then((res)=>{
   console.log(res);
-});
+})
+.catch((err)=>{
+  console.log(err);
+})
