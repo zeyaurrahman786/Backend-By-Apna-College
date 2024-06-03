878465d4ffc40900dcc59b2d7939c3c7d0fc5c7f
zeyaurrahman786 <zeyaurrahman078608@gmail.com>
2024-06-03 23:14:37 +0530

    MongoDB

--- MongoDB/index.js
+++ MongoDB/index.js
@@ -19,10 +19,41 @@ const userSchema = new mongoose.Schema ({
 
 const User = mongoose.model("User", userSchema);
 
-const User1 = new User ({
-  name: "Rahul",
-  email: "rahul@gmail.com",
-  age: 20,
-});
 
-User1.save();
+// const User1 = new User ({
+//   name: "Rahul",
+//   email: "rahul@gmail.co
+//   age: 20,
+// });
+
+// User1.save();
+
+
+
+// const User2 = new User({
+//   name: "Zeyaur",
+//   email: "zeyaur@gmail.com",
+//   age: 21,
+// });
+
+// User2
+// .save()
+// .then((res)=>{
+//   console.log(res);
+// })
+// .catch((err)=>{
+//   console.log(err);
+// }); 
+
+
+
+User.insertMany([
+  {name: 'John', email : 'john@example.com', age: 50},
+  {name: 'Tony', email : 'tony@example.com', age: 40},
+  {name: 'Peter', email : 'peter@example.com', age: 45},
+  {name: 'Bruce', email : 'bruce@example.com', age: 55},
+  {name: 'Adam', email : 'adam@example.com', age: 60},
+])
+.then((res) => {
+  console.log(res);
+});
