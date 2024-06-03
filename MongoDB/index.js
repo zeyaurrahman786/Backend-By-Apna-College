891323e5a53a2769c8d75172ed7752f267ba90e0
zeyaurrahman786 <zeyaurrahman078608@gmail.com>
2024-06-03 22:38:07 +0530

    MongoDB

--- MongoDB/index.js
+++ MongoDB/index.js
@@ -1,15 +1,28 @@
-// getting-started.js
 const mongoose = require('mongoose');
 
 main()
 .then(() => {
     console.log('Connected to MongoDB Sucessfully');
-    mongoose.connection.close();
 })
 .catch(err => console.log(err)); 
 
 async function main() {
   await mongoose.connect('mongodb://127.0.0.1:27017/test');
-
-  // use `await mongoose.connect('mongodb://user:password@127.0.0.1:27017/test');` if your database has auth enabled
 }
+
+
+const userSchema = new mongoose.Schema ({
+  name: String,
+  email: String,
+  age: Number,
+});
+
+const User = mongoose.model("User", userSchema);
+
+const User1 = new User ({
+  name: "Rahul",
+  email: "rahul@gmail.com",
+  age: 20,
+});
+
+User1.save();
